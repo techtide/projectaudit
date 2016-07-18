0f9ec0f88dcbd0769ae1e6491171cc0e3cf430b7
Sam Walters <armanbhalla6@gmail.com>
2016-07-18 22:44:20 +0100

    Worked on main menu. Added project fields, and worked on add project menu.

---
+++ src/project/audit/Project.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package project.audit;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.LinkedList;
+
+/**
+ *
+ * @author Greenie
+ */
+public class Project {
+
+    List<String> projectName = new ArrayList<>();
+    List<Integer> projectBudget = new ArrayList<>();
+    List<String> projectWebsite = new ArrayList<>();
+    List<String> projectCollaborators = new ArrayList<>();
+    List<String> projectDescription = new ArrayList<>();
+}
